ac09f5da54be50c8cc264ee7cd23e71ec3c3c8e1
lucky-y <heyuan@huniutech.com>
2019-08-08 16:22:23 +0800

    building-env

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id='app'>
+    <img src='./assets/logo.png' />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      naem: '',
+      age: ''
+    }
+  },
+  created: () => {
+    let api = process.env.BASE_API
+    console.log(api)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
